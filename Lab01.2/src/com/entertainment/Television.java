f106c2e232f385e0e0cedddf4699fc4700855fa5
Chad Kellum <chad.kellum@tlgcohort.com>
2023-02-28 10:14:59 -0800

    Refinement of equals()

--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -44,11 +44,29 @@ public class Television {
     public void setVolume(int volume) {
         this.volume = volume;
     }
+
     @Override
+    public boolean equals(Object obj) {
+        // if I am the same physical object as obj
+        if (this == obj) return true;
+
+        // if obj is null OR I and obj are not the same EXACT type
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Television that = (Television) obj;
+        return this.getVolume() == that.getVolume() &&
+               Objects.equals(this.getBrand(), that.getBrand());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBrand(), getVolume());
+    }
+
+    /* @Override
     public boolean equals(Object obj) {
         boolean result = false;
 
-        if (obj instanceof Television) {
+        if (this.getClass() == obj.getClass()) {
             Television other = (Television) obj;
             result = Objects.equals(this.getBrand(), other.getBrand()) &&  // null-safe check
                      this.getVolume() == other.getVolume(); // int can't be null
@@ -61,7 +79,7 @@ public class Television {
         // a "hash collision" is when different objects (by equals() have the SAME hashcode by coincidence.
 //        return getBrand().length() + getVolume();
         return Objects.hash(getBrand(), getVolume());
-    }
+    }*/
 
     public String toString() {
         return getClass().getSimpleName() + " [brand=" + getBrand() + ", volume=" +
